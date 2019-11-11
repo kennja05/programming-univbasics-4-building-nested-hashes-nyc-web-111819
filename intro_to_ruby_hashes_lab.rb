9ab5fd864c236dd9fba3e064a104c7de88f44963
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-11 23:36:10 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -3,10 +3,11 @@ def base_hash
 end
 
 def monopoly_with_second_tier
-  {:railroads => {:pieces => 4}}
+  {:railroads => :pieces => 4}
 end
 
 def monopoly_with_third_tier
+  
 end
 
 def monopoly_with_fourth_tier
