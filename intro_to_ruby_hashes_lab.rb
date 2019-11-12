0208f55555111904262e45642d62acc7f5ed75fb
Jacob Kenny <jacobkenny05@gmail.com>
2019-11-12 00:50:16 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -7,7 +7,7 @@ def monopoly_with_second_tier
 end
 
 def monopoly_with_third_tier
-  {:railroads => {:names => {:reading_railroad => {},:two => "2nd placeholder",:three => "third placeholder",:four => "fourth placeholder"}, :rent_in_dollars => {:one_piece_owned => 25,:two_pieces_owned => 50,:three_pieces_owned => 100,:four_pieces_owned => 200}}} 
+  {:railroads => {:names => {:reading_railroad => {},:pennsylvania_railroad => {},:three => "third placeholder",:four => "fourth placeholder"}, :rent_in_dollars => {:one_piece_owned => 25,:two_pieces_owned => 50,:three_pieces_owned => 100,:four_pieces_owned => 200}}} 
 end
 
 def monopoly_with_fourth_tier
